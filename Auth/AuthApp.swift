ddb56bb8b6d17d0fef188c14bf3430fca22f75d1
busragedikoglu <bgedikogluu@gmail.com>
2024-08-21 10:59:36 +0300

    Initial Commit

---
+++ Auth/AuthApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthApp.swift
+//  Auth
+//
+//  Created by Büşra Gedikoğlu on 21.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct AuthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
